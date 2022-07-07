cbf67312514d873b38dcfc7d9a3452e4902cda3b
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-07-06 22:13:21 -0400

    added login screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Dashboard from "./components/dashboard/Dashboard";
+import Login from "./components/login/Login";
 function App() {
+  // user state
+  const [user, setUser] = useState(null);
+
+  //autologin
+  useEffect(() => {
+    autoLogin();
+  }, []);
+
+  //auto login fetch
+  const autoLogin = () => {
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => {
+          setUser(user);
+        });
+      }
+    });
+  };
+
+  // show login screen is null (falsy)
+  if (!user)
+    return (
+      <div>
+        <Login onLogin={setUser} />
+      </div>
+    );
+
+  // signout function
+  const handleLogoutClick = () => {
+    fetch("/logout", { method: "DELETE" }).then((r) => {
+      if (r.ok) {
+        setUser(null);
+
+        // window.location.reload(false);
+      }
+    });
+  };
+
+  // show dashboard if logged in
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Dashboard handleLogoutClick={handleLogoutClick} user={user} />
     </div>
   );
 }
-
 export default App;
---
+++ client/src/components/login/Login.js
@@ -0,0 +1,105 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#6320ee",
+      contrastText: "white",
+    },
+  },
+  Typography: {
+    fontFamily: "Exo",
+  },
+});
+
+function Login({ onLogin, fetchCartLength, fetchBots }) {
+  const [showLogin, setShowLogin] = useState(true);
+  //   logout
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: showLogin
+              ? "url(https://dribblemedia.com/wp-content/uploads/2021/04/The-20-DM.jpg)"
+              : "url(https://wallpaperaccess.com/full/709458.jpg)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "center",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={3} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h5" sx={{ display: { md: "none" } }}>
+              Dream.
+            </Typography>
+            <Paper sx={{ mt: 10, elevation: 2 }}>
+              {showLogin ? (
+                <SignIn
+                  onLogin={onLogin}
+                  showLogin={showLogin}
+                  setShowLogin={setShowLogin}
+                />
+              ) : (
+                <>
+                  <SignUp
+                    onLogin={onLogin}
+                    showLogin={showLogin}
+                    setShowLogin={setShowLogin}
+                  />
+                </>
+              )}
+              <Button
+                sx={{ ml: 2 }}
+                onClick={() => {
+                  setShowLogin(!showLogin);
+                }}
+              >
+                {showLogin
+                  ? "Don't have an account? Sign up here"
+                  : "Already have an account? Sign in here"}
+              </Button>
+            </Paper>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
+
+export default Login;
