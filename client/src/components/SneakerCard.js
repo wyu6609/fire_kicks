7b8e41029962c42ca87b0d807ab2842defc2070f
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-07-14 20:53:46 -0400

    ui changes

--- client/src/components/SneakerCard.js
+++ client/src/components/SneakerCard.js
@@ -3,6 +3,10 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Accordion from "@mui/material/Accordion";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { CardActionArea } from "@mui/material";
 
 export default function ActionAreaCard({ sneaker }) {
@@ -36,9 +40,22 @@ export default function ActionAreaCard({ sneaker }) {
         <Typography gutterBottom align="center" variant="h5" component="div">
           {sneaker.name}
         </Typography>
-        <Typography variant="p" color="text.secondary">
+        {/*<Typography variant="p" color="text.secondary">
           {sneaker.description}
-        </Typography>
+        </Typography> */}
+
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            {sneaker.official_colorway}
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>{sneaker.description}</Typography>
+          </AccordionDetails>
+        </Accordion>
       </CardContent>
     </Card>
   );
